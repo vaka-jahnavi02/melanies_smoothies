c58c0c743317371299a2563cac66b7a424cb69df
vaka-jahnavi02 <133942925+vaka-jahnavi02@users.noreply.github.com>
2025-11-14 18:30:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ my_dataframe= session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_
 #st.dataframe(data= my_dataframe,use_container_width=True)
 #st.stop()
 pd_df= my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 ingredients_list= st.multiselect(
     'Choose upto 5 Ingredients: ', my_dataframe,max_selections=5
 )
