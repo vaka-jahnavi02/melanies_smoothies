7221808b8c60cf14cf1726452c942029f36320ba
vaka-jahnavi02 <133942925+vaka-jahnavi02@users.noreply.github.com>
2025-11-14 18:00:55 +0530

    Update streamlit_app.py
    Added the 2 lines in fruit_chosen function

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,9 @@ if ingredients_list:
     ingredients_string=''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        #st.text(smoothiefroot_response)
+        sf_df= st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
     #st.write(ingredients_string)
 
     my_insert_stmt= """insert into smoothies.public.orders(ingredients,name_on_order)
